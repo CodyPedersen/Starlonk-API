cff6d7c4c2d06fd97cc50e3c407b4b029107566d
CodyPedersen <test@test.com>
2023-06-13 19:13:01 -0400

    lint main

--- main.py
+++ main.py
@@ -1,31 +1,30 @@
-from fastapi import FastAPI, Request, Depends, Header, BackgroundTasks, status
+"""Main - houses REST API endpoint routers"""
+import uuid
 from typing import Union
 from datetime import datetime, timedelta
+
 from sqlalchemy.orm import Session
+from ariadne import graphql_sync
+from gql import schema
+from fastapi import FastAPI, Request, Depends, Header, BackgroundTasks
+
 from utils.database import engine, get_db
 from utils.data import refresh_satellite_data, log_data
 from api.schemas import SatelliteQuery
 from api.auth import authorize
 import utils.predict as predict
 import utils.models as models
-import json
-
-# GraphQL
-from ariadne import graphql_sync
-from ariadne.constants import PLAYGROUND_HTML
-from gql import query, mutation, type_defs, schema
-
-import uuid
 
 # Create DB tables if DNE
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
+
 @app.get("/")
 async def get_home():
     """Currently inactive"""
-    return {"message" : "Starlink API. See /docs"}
+    return {"message": "Starlink API. See /docs"}
 
 
 @app.get("/satellites/")
@@ -34,7 +33,8 @@ async def get_satellites(db: Session = Depends(get_db), params: SatelliteQuery =
 
     param_dict = params.dict(exclude_none=True)
 
-    query = db.query(models.Satellite) # Not query.all() as that returns a list
+    # Not query.all() as that returns a list
+    query = db.query(models.Satellite)
 
     # Filter list on each attribute in parameters
     for key, val in param_dict.items():
@@ -42,9 +42,9 @@ async def get_satellites(db: Session = Depends(get_db), params: SatelliteQuery =
 
     satellite_list = [s.to_dict() for s in query.all()]
 
-    return {"satellites" : satellite_list}
+    return {"satellites": satellite_list}
 
-   
+
 @app.get("/predict/")
 async def predict_satellites(db: Session = Depends(get_db), params: SatelliteQuery = Depends()):
     """Predict the location of a single satellite for a given epoch"""
@@ -52,9 +52,10 @@ async def predict_satellites(db: Session = Depends(get_db), params: SatelliteQue
     param_dict = params.dict(exclude_none=True)
     satellite_id = param_dict['satellite_id']
     epoch = param_dict['epoch']
-    
+
     # Get satellite
-    satellite = db.query(models.Satellite).filter(models.Satellite.satellite_id == satellite_id).first()
+    satellite = db.query(models.Satellite).filter(
+        models.Satellite.satellite_id == satellite_id).first()
 
     satellite_prediction = predict.predict_location(satellite, epoch)
 
@@ -62,7 +63,7 @@ async def predict_satellites(db: Session = Depends(get_db), params: SatelliteQue
 
 
 @app.get("/bulk_predict/")
-async def get_satellites(db: Session = Depends(get_db), params: SatelliteQuery = Depends()):
+async def get_bulk_satellites(db: Session = Depends(get_db), params: SatelliteQuery = Depends()):
     """Predict all satellite location for a given epoch"""
 
     param_dict = params.dict(exclude_none=True)
@@ -77,12 +78,16 @@ async def get_satellites(db: Session = Depends(get_db), params: SatelliteQuery =
         satellite_prediction = predict.predict_location(satellite, epoch)
         satellite_predictions.append(satellite_prediction)
 
-    return {"satellites" : satellite_predictions}
+    return {"satellites": satellite_predictions}
 
 
 @app.post("/admin/refresh/", status_code=202)
 @authorize
-async def refresh_data(background_tasks : BackgroundTasks, db: Session = Depends(get_db), Authorization: Union[str, None] = Header(default=None)):
+async def refresh_data(
+    background_tasks: BackgroundTasks,
+    db: Session = Depends(get_db),
+    Authorization: Union[str, None] = Header(default=None)
+):
     """Pull data from NORAD data sets and update DB with data"""
 
     pid = str(uuid.uuid4())
@@ -90,18 +95,23 @@ async def refresh_data(background_tasks : BackgroundTasks, db: Session = Depends
     log_data("Refreshed satellite data")
 
     return {
-        "status": "accepted", 
-        "pid" : pid
+        "status": "accepted",
+        "pid": pid
     }
 
 
 @app.get("/admin/process/{process_id}")
 @authorize
-async def get_process(process_id, db: Session = Depends(get_db), Authorization: Union[str, None] = Header(default=None)):
+async def get_process(
+    process_id,
+    db: Session = Depends(get_db),
+    Authorization: Union[str, None] = Header(default=None)
+    ):
     """Get the status given process (refresh)"""
-    
-    process = db.query(models.Process).filter(models.Process.id == process_id).one()
-    return {"process" : process.to_dict()}
+
+    process = db.query(models.Process).filter(
+        models.Process.id == process_id).one()
+    return {"process": process.to_dict()}
 
 
 @app.post("/admin/purge")
@@ -112,36 +122,31 @@ async def purge_old(db: Session = Depends(get_db), Authorization: Union[str, Non
     utc_cutoff = (datetime.utcnow() - timedelta(days=14)).isoformat()
 
     # Find satellites
-    satellites = db.query(models.Satellite).filter(models.Satellite.epoch < utc_cutoff).all()
-    deletes = [satellite.to_dict()["satellite_name"] for satellite in satellites]
+    satellites = db.query(models.Satellite).filter(
+        models.Satellite.epoch < utc_cutoff).all()
+    deletes = [satellite.to_dict()["satellite_name"]
+               for satellite in satellites]
 
     # Delete satellites
-    satellites = db.query(models.Satellite).filter(models.Satellite.epoch < utc_cutoff).delete()
+    satellites = db.query(models.Satellite).filter(
+        models.Satellite.epoch < utc_cutoff).delete()
     db.commit()
 
-    return {"deleted" : deletes}
-
-
-''' GraphQL Endpoints '''
-
-# @app.get("/graphql/")
-# async def graphql_playground():
-#     return PLAYGROUND_HTML, 200
+    return {"deleted": deletes}
 
 
 @app.post("/graphql/")
 async def graphql_fn(req: Request):
-    data = await req.json() #JSON request data
+    """GraphQL default endpoint routing"""
+    data = await req.json()  # JSON request data
     #request_json = data.decode('ascii')
-    print(data)
-
-    # Use executable schema with user GQL query to generate result
-    _, result = graphql_sync( 
+    #print(data)
+    _, result = graphql_sync(
         schema,
         data,
         context_value=data,
         debug=app.debug
-    ) 
+    )
     #status_code = 200 if success else 400
     return result
 
