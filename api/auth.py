904667d5054551ebd6985ddf71f8aa365fb44b66
Cody Pedersen <enso@Codys-MacBook-Pro.local>
2024-05-10 19:08:42 -0400

    Update authorize

--- api/auth.py
+++ api/auth.py
@@ -2,13 +2,14 @@
 import os
 from functools import wraps
 
+from fastapi import HTTPException
+
+
 def authorize(f):
     """Decorator to force API key authentication/authorization on API call"""
     @wraps(f)
-    async def resolver(Authorization, *args, **kwargs):
-        print("Authorizing")
+    async def resolver(Authorization: str, *args, **kwargs):
         if Authorization != os.getenv('API_KEY'):
-            return {"status": "Invalid credentials"}
-
+            raise HTTPException(status_code=401, detail="Authorization failed")
         return await f(*args, **kwargs)
     return resolver
